154bd9b4672ca9097b83a03b20679c38b654d5f3
Ståle Heitmann <ssheitmann@gmail.com>
2021-07-09 13:34:10 +0200

    Use sarama instead of confluent.

--- producer.go
+++ producer.go
@@ -1,12 +1,14 @@
 package ednaevents
 
 import (
-	"fmt"
+	"context"
+	"encoding/json"
 	"github.com/3lvia/telemetry-go"
+	"github.com/Shopify/sarama"
+	"github.com/cloudevents/sdk-go/protocol/kafka_sarama/v2"
 	cloudevents "github.com/cloudevents/sdk-go/v2"
-	"github.com/confluentinc/confluent-kafka-go/kafka"
+	//"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/google/uuid"
-	"log"
 	"time"
 )
 
@@ -22,58 +24,85 @@ const (
 )
 
 type producer struct {
-	kConfig         *kafka.ConfigMap
+	//kConfig         *kafka.ConfigMap
 	schema          string
 	schemaReference string
 	config          *Config
 	logChannels     telemetry.LogChannels
 }
 
-func (p *producer) start(ch <-chan *Message) {
-	kProducer, err := kafka.NewProducer(p.kConfig)
-	defer kProducer.Close()
+func (p *producer) start(ctx context.Context, ch <-chan *Message) {
+	//kProducer, err := kafka.NewProducer(p.kConfig)
+	//defer kProducer.Close()
+	//if err != nil {
+	//	p.logChannels.ErrorChan <- err
+	//	return
+	//}
+
+	//go func() {
+	//	for e := range kProducer.Events() {
+	//		switch ev := e.(type) {
+	//		case *kafka.Message:
+	//			m := ev
+	//			if m.TopicPartition.Error != nil {
+	//				p.logChannels.CountChan <- telemetry.Metric{
+	//					Name:        metricCountUndelivered,
+	//					Value:       1,
+	//					ConstLabels: map[string]string{"day": dayKey(time.Now()), "topic": *m.TopicPartition.Topic},
+	//				}
+	//				log.Printf("Delivery failed: %v\n", m.TopicPartition.Error)
+	//			} else {
+	//				p.logChannels.CountChan <- telemetry.Metric{
+	//					Name:        metricCountDelivered,
+	//					Value:       1,
+	//					ConstLabels: map[string]string{
+	//						"day": dayKey(time.Now()),
+	//						"topic": *m.TopicPartition.Topic,
+	//						"partition": fmt.Sprintf("%d", m.TopicPartition.Partition),
+	//					},
+	//				}
+	//				log.Printf("Delivered message to topic %s [%d] at offset %v\n",
+	//					*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
+	//			}
+	//			return
+	//
+	//		default:
+	//			p.logChannels.CountChan <- telemetry.Metric{
+	//				Name:        metricCountIgnored,
+	//				Value:       1,
+	//				ConstLabels: map[string]string{"day": dayKey(time.Now()), "topic": p.config.Topic},
+	//			}
+	//			log.Printf("Ignored event: %s\n", ev)
+	//		}
+	//	}
+	//}()
+
+	saramaConfig := sarama.NewConfig()
+	saramaConfig.Version = sarama.V2_8_0_0
+	saramaConfig.Net.SASL.User = p.config.Username
+	saramaConfig.Net.SASL.Password = p.config.Password
+	saramaConfig.Net.SASL.Mechanism = "PLAIN"
+
+	//kConfig := &kafka.ConfigMap{
+	//	"bootstrap.servers": c.Broker,
+	//	"sasl.username":     c.Username,
+	//	"sasl.password":     c.Password,
+	//	"sasl.mechanism":    "PLAIN",
+	//	"security.protocol": "SASL_SSL",
+	//}
+
+	sender, err := kafka_sarama.NewSender([]string{p.config.Broker}, saramaConfig, p.config.Topic)
 	if err != nil {
 		p.logChannels.ErrorChan <- err
 		return
 	}
+	defer sender.Close(ctx)
 
-	go func() {
-		for e := range kProducer.Events() {
-			switch ev := e.(type) {
-			case *kafka.Message:
-				m := ev
-				if m.TopicPartition.Error != nil {
-					p.logChannels.CountChan <- telemetry.Metric{
-						Name:        metricCountUndelivered,
-						Value:       1,
-						ConstLabels: map[string]string{"day": dayKey(time.Now()), "topic": *m.TopicPartition.Topic},
-					}
-					log.Printf("Delivery failed: %v\n", m.TopicPartition.Error)
-				} else {
-					p.logChannels.CountChan <- telemetry.Metric{
-						Name:        metricCountDelivered,
-						Value:       1,
-						ConstLabels: map[string]string{
-							"day": dayKey(time.Now()),
-							"topic": *m.TopicPartition.Topic,
-							"partition": fmt.Sprintf("%d", m.TopicPartition.Partition),
-						},
-					}
-					log.Printf("Delivered message to topic %s [%d] at offset %v\n",
-						*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
-				}
-				return
-
-			default:
-				p.logChannels.CountChan <- telemetry.Metric{
-					Name:        metricCountIgnored,
-					Value:       1,
-					ConstLabels: map[string]string{"day": dayKey(time.Now()), "topic": p.config.Topic},
-				}
-				log.Printf("Ignored event: %s\n", ev)
-			}
-		}
-	}()
+	client, err := cloudevents.NewClient(sender, cloudevents.WithTimeNow(), cloudevents.WithUUIDs())
+	if err != nil {
+		p.logChannels.ErrorChan <- err
+		return
+	}
 
 	for {
 		obj := <-ch
@@ -84,13 +113,17 @@ func (p *producer) start(ch <-chan *Message) {
 			continue
 		}
 
-		js, err := ce.MarshalJSON()
-		if err != nil {
-			p.logChannels.ErrorChan <- err
-			continue
-		}
+		result := client.Send(ctx, ce)
+		_ = result
 
-		kProducer.ProduceChannel() <- &kafka.Message{TopicPartition: kafka.TopicPartition{Topic: &p.config.Topic, Partition: kafka.PartitionAny}, Value: js}
+		//js, err := ce.MarshalJSON()
+		//if err != nil {
+		//	p.logChannels.ErrorChan <- err
+		//	continue
+		//}
+		//_ = js
+
+		//kProducer.ProduceChannel() <- &kafka.Message{TopicPartition: kafka.TopicPartition{Topic: &p.config.Topic, Partition: kafka.PartitionAny}, Value: js}
 	}
 }
 
@@ -113,12 +146,18 @@ func (p *producer) getCloudEvent(m *Message) (cloudevents.Event, error) {
 	ce.SetType(p.config.Type)
 	ce.SetDataSchema(p.schemaReference)
 
-	b, err := serialize(m.Payload, p.schema)
+	//b, err := serialize(m.Payload, p.schema)
+	//if err != nil {
+	//	return cloudevents.Event{}, err
+	//}
+
+	b, err := json.Marshal(m.Payload)
 	if err != nil {
-		return cloudevents.Event{}, err
+		return ce, err
 	}
 
-	ce.SetData(contentType, b)
+	//ce.SetData(contentType, b)
+	ce.SetData("application/json", b)
 
 	return ce, nil
 }
